81b1c5af666e467d9898815decc5471b2518cf34
fahed2342 <fahed12alsa@gmail.com>
2025-05-22 01:06:03 +0300

    adding new project

---
+++ countdown-project/newYear-Countdown.js
@@ -0,0 +1,46 @@
+    const dayEl = document.getElementById("day");
+    const hourEl = document.getElementById("hour");
+    const minuteEl = document.getElementById("minute");
+    const secondEl = document.getElementById("second");
+
+    const newYearTime = new Date("Jan 1, 2026 00:00:00").getTime();
+
+    updateCountdown();
+
+    function updateCountdown() {
+    const now = new Date().getTime();
+    const gap = newYearTime - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const d = Math.floor(gap / day);
+    const h = Math.floor((gap % day) / hour);
+    const m = Math.floor((gap % hour) / minute);
+    const s = Math.floor((gap % minute) / second);
+    dayEl.innerText = d;
+    hourEl.innerText = h;
+    minuteEl.innerText = m;
+    secondEl.innerText = s;
+    setTimeout(updateCountdown, 1000)
+    }
+
+
+    const bodyEl = document.querySelector("body");
+
+    bodyEl.addEventListener("mousemove", (event) => {
+    const xPos = event.offsetX;
+    const yPos = event.offsetY;
+    const spanEl = document.createElement("span");
+    spanEl.style.left = xPos + "px";
+    spanEl.style.top = yPos + "px";
+    const size = Math.random() * 100;
+    spanEl.style.width = size + "px";
+    spanEl.style.height = size + "px";
+    bodyEl.appendChild(spanEl);
+    setTimeout(() => {
+        spanEl.remove();
+    }, 3000);
+    });
